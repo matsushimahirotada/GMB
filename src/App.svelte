b81738297e6b7aa975ce34c3c5a2575f65b31068
matsushimahirotada <t170d516@gunma-u.ac.jp>
2021-12-03 03:59:40 +0900

    ボタン作ってみた

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  export let name: string;
+
+  function click1() {
+    alert("1に投票されました");
+  }
+
+  function click2() {
+    alert("2に投票されました");
+  }
+</script>
+
+<Tailwindcss />
+<main>
+  <p><button class="btn btn-blue" on:click={click1}>1に投票</button></p>
+  <p><button class="btn btn-blue" on:click={click2}>2に投票</button></p>
+</main>
+
+<style>
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
